c3929864d80b1499cd7f5229425565668d826b8b
Cryllicbuster273 <cryllicbuster273@gmail.com>
2020-05-23 20:57:20 +0200

    phoenix: PixysOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_phoenix.mk
+    $(LOCAL_DIR)/pixys_phoenix.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_phoenix-user \
-    lineage_phoenix-userdebug \
-    lineage_phoenix-eng
+    pixys_phoenix-user \
+    pixys_phoenix-userdebug \
+    pixys_phoenix-eng
