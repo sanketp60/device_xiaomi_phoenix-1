2780c5f25bafa42a4cd05c60548492a9818dba10
Pig <pig.priv@gmail.com>
2020-06-15 14:12:39 +0200

    sm6150-common: Use ODM_MANIFEST_FILES for QVA manifest
     * It will only be loaded if the sku is set to qva with current way.
     * This commit partially reverts d954a8d4daba82d2fc0f46bf949a5c5bc8fc83a2.
    
    Change-Id: I1dda71925f220a4352277a242902cd9d9ee41cbe
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,12 +140,12 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += \
 	365 \
-	366 \
-	qva
+	366
 
 ODM_MANIFEST_365_FILES := $(DEVICE_PATH)/manifest_sdmmagpie.xml
 ODM_MANIFEST_366_FILES := $(DEVICE_PATH)/manifest_sdmmagpie.xml
-ODM_MANIFEST_QVA_FILES := $(DEVICE_PATH)/manifest-qva.xml
+
+ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest-qva.xml
 
 # LMKD
 TARGET_LMKD_STATS_LOG := true
